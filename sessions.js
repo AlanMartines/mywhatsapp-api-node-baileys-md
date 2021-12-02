dd783eea88dd90e2955d68c39e731caf822f10fd
Alan Martines <alancpmartines@hotmail.com>
2021-12-01 23:42:37 -0400

    Inicial Update

--- sessions.js
+++ sessions.js
@@ -6,6 +6,10 @@ const QRCode = require('qrcode');
 const qrcodeterminal = require('qrcode-terminal');
 const moment = require("moment");
 const pino = require("pino");
+const {
+  Boom
+}
+fro = require("@hapi/boom");
 const Base64BufferThumbnail = require('base64-buffer-thumbnail');
 const {
   fromPath,
