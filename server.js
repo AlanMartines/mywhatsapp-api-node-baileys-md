3a338828359be47d400fa53e5a3908f5001c1d4b
Alan Martines <alancpmartines@hotmail.com>
2021-12-07 12:25:04 -0400

    Inicial Update

--- server.js
+++ server.js
@@ -16,51 +16,51 @@ fs.access(".env", fs.constants.F_OK, (err) => {
     if (err.code === 'ENOENT') {
       console.error('- Arquivo ".env');
       var modelo = `
-			NODE_EN=production
-			#
-			# Defina o HOST aqui caso voce utilize uma VPS deve ser colocado o IP da VPS
-			# Exemplos:
-			# HOST=204.202.54.2 => IP da VPS, caso esteja usando virtualização via hospedagem
-			# HOST=10.0.0.10 => IP da VM, caso esteja usando virtualização
-			# HOST=localhost => caso esteja usando na sua proprima maquina local
-			HOST=localhost
-			#
-			# Defina o numero da porta a ser usada pela API.
-			PORT=9003
-			#
-			# Define se o qrcode vai ser mostrado no terminal
-			VIEW_QRCODE_TERMINAL=0
-			#
-			# Chave de segurança para validação no JWT
-			JWT_SECRET=09f26e402586e2faa8da4c98a35f1b20d6b033c60
-			#
-			# Diretório da pasta onde irá salvar os tokens gerados no linux
-			TOKENSPATCH=/usr/local/tokens
-			#
-			# Validate in terminal false or true
-			VALIDATE_MYSQL=0
-			#
-			# O host do banco. Ex: localhost
-			MYSQL_HOST=localhost
-			#
-			# Port do banco. Ex: 3306
-			MYSQL_PORT=3306
-			#
-			# Um usuário do banco. Ex: user
-			MYSQL_USER=mywhatsappapi
-			#
-			# A senha do usuário do banco. Ex: user123
-			MYSQL_PASSWORD=TuUep8KkjCtAA@
-			#
-			# A senha do usuário root do banco. Ex: root123
-			MYSQL_ROOT_PASSWORD=TuUep8KkjCtAA@
-			#
-			# A base de dados a qual a aplicação irá se conectar. Ex: node_mysql
-			MYSQL_DATABASE=mywhatsapp-api
-			#
-			# Criando volume onde irá salvar os tokens gerados
-			VOLUME=/usr/local/tokens
-			#
+NODE_EN=production
+#
+# Defina o HOST aqui caso voce utilize uma VPS deve ser colocado o IP da VPS
+# Exemplos:
+# HOST=204.202.54.2 => IP da VPS, caso esteja usando virtualização via hospedagem
+# HOST=10.0.0.10 => IP da VM, caso esteja usando virtualização
+# HOST=localhost => caso esteja usando na sua proprima maquina local
+HOST=localhost
+#
+# Defina o numero da porta a ser usada pela API.
+PORT=9003
+#
+# Define se o qrcode vai ser mostrado no terminal
+VIEW_QRCODE_TERMINAL=0
+#
+# Chave de segurança para validação no JWT
+JWT_SECRET=09f26e402586e2faa8da4c98a35f1b20d6b033c60
+#
+# Diretório da pasta onde irá salvar os tokens gerados no linux
+TOKENSPATCH=/usr/local/tokens
+#
+# Validate in terminal false or true
+VALIDATE_MYSQL=0
+#
+# O host do banco. Ex: localhost
+MYSQL_HOST=localhost
+#
+# Port do banco. Ex: 3306
+MYSQL_PORT=3306
+#
+# Um usuário do banco. Ex: user
+MYSQL_USER=mywhatsappapi
+#
+# A senha do usuário do banco. Ex: user123
+MYSQL_PASSWORD=TuUep8KkjCtAA@
+#
+# A senha do usuário root do banco. Ex: root123
+MYSQL_ROOT_PASSWORD=TuUep8KkjCtAA@
+#
+# A base de dados a qual a aplicação irá se conectar. Ex: node_mysql
+MYSQL_DATABASE=mywhatsapp-api
+#
+# Criando volume onde irá salvar os tokens gerados
+VOLUME=/usr/local/tokens
+#
 `;
       console.log("- Modelo do arquivo de configuração:\n", modelo);
       process.exit(1);
