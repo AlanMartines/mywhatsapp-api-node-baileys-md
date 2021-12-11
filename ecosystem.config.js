e6a3a0244b77925fc6f4ce5e55dcbf321d03b7cc
Alan Martines <alancpmartines@hotmail.com>
2021-12-11 13:12:06 -0400

    Inicial Update

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,19 +2,18 @@
 //
 require('dotenv/config');
 //
-const HOST = process.env.NODE_ENV;
-const HOST = process.env.HOST || localhost;
-const PORT = process.env.PORT;
-const VIEW_QRCODE_TERMINAL = process.env.VIEW_QRCODE_TERMINAL;
+const NODE_ENV = process.env.NODE_ENV || 'production';
+const HOST = process.env.HOST || 'localhost';
+const PORT = process.env.PORT || '9001';
+const VIEW_QRCODE_TERMINAL = process.env.VIEW_QRCODE_TERMINAL || 1;
 const JWT_SECRET = process.env.JWT_SECRET;
-const TOKENSPATCH_LINUX = process.env.TOKENSPATCH_LINUX;
-const TOKENSPATCH_WIN = process.env.TOKENSPATCH_WIN;
-const VALIDATE_MYSQL = process.env.VALIDATE_MYSQL;
-const HOST_MYSQL = process.env.HOST_MYSQL;
-const PORT_MYSQL = process.env.PORT_MYSQL;
-const USER_MYSQL = process.env.USER_MYSQL;
-const PASSWORD_MYSQL = process.env.PASSWORD_MYSQL;
-const DATABASE_MYSQL = process.env.DATABASE_MYSQL;
+const TOKENSPATCH = process.env.TOKENSPATCH || 'tokens';
+const VALIDATE_MYSQL = process.env.VALIDATE_MYSQL || 0;
+const MYSQL_HOST = process.env.MYSQL_HOST || 'localhost';
+const MYSQL_PORT = process.env.MYSQL_PORT || '3306';
+const MYSQL_USER = process.env.MYSQL_USER || 'root';
+const MYSQL_PASSWORD = process.env.MYSQL_PASSWORD || '';
+const MYSQL_DATABASE = process.env.MYSQL_DATABASE || '';
 //
 module.exports = {
   apps: [{
@@ -29,14 +28,14 @@ module.exports = {
       PORT,
       VIEW_QRCODE_TERMINAL,
       JWT_SECRET,
-      TOKENSPATCH_LINUX,
-      TOKENSPATCH_WIN,
+      TOKENSPATCH,
       VALIDATE_MYSQL,
-      HOST_MYSQL,
+      MYSQL_HOST,
       PORT_MYSQL,
-      USER_MYSQL,
-      PASSWORD_MYSQL,
-      DATABASE_MYSQL
+      MYSQL_PORT,
+      MYSQL_USER,
+      MYSQL_PASSWORD,
+      MYSQL_DATABASE
     },
   }]
 }
