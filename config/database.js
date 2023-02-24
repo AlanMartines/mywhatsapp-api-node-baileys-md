1cdcc5fee7b71f9a13225597bbc24998b1f24760
Alan Martines <alancpmartines@hotmail.com>
2023-02-24 08:39:26 -0400

    Udate

--- config/database.js
+++ config/database.js
@@ -1,26 +1,29 @@
 const config = require('../config.global');
+//
 module.exports = {
-	dialect: config.MYSQL_DIALECT,
-	host: config.MYSQL_HOST,
-	port: config.MYSQL_PORT,
-	username: config.MYSQL_USER,
-	password: config.MYSQL_PASSWORD,
-	database: config.MYSQL_DATABASE,
-	logging: false,
-	define: {
-		timestamps: false,
-		underscored: true,
-		underscoredAll: true,
-		freezeTableName: false,
-		syncOnAssociation: true,
-		charset: 'utf8',
-		collate: 'utf8_general_ci'
-	},
-	pool: {
-		max: 15,
-		min: 5,
+  dialect: config.MYSQL_DIALECT,
+  host: config.MYSQL_HOST,
+  port: config.MYSQL_PORT,
+  username: config.MYSQL_USER,
+  password: config.MYSQL_PASSWORD,
+  database: config.MYSQL_DATABASE,
+  logging: false,
+  define: {
+    timestamps: false,
+    underscored: true,
+    underscoredAll: true,
+    freezeTableName: false,
+    syncOnAssociation: true,
+    charset: 'utf8',
+    collate: 'utf8_general_ci'
+  },
+  pool: {
+    max: 30,
+    min: 5,
 		idle: 20000,
 		evict: 15000,
 		acquire: 30000
-	}
-}
+	},
+	timezone: '-03:00'
+};
+//
--- utils/logger.js
+++ utils/logger.js
@@ -1,18 +1,33 @@
 const pino = require("pino");
 const pretty = require('pino-pretty');
-//
-// https://github.com/pinojs/pino-pretty
+
 const logger = pino({
   timestamp: false,
+  levelFirst: true,
   transport: {
     target: 'pino-pretty',
     options: {
-    errorProps: '*',
-		ignore: "pid,hostname",
-		translateTime: "SYS:yyyy-mm-dd HH:MM:ss",
-    colorize: true
+      errorProps: '*',
+      ignore: "pid,hostname",
+      translateTime: "SYS:yyyy-mm-dd HH:MM:ss",
+      colorize: true
     }
   }
+}, {
+  // Opções de log de erro
+  level: 'error',
+  messageKey: 'message',
+  formatters: {
+    level(label, number) {
+      return { level: label };
+    },
+    error(error) {
+      return {
+        stack: error.stack, // exibe nome do arquivo e linha do erro
+        message: error.message,
+      };
+    },
+  },
 });
-//
+
 exports.logger = logger;
