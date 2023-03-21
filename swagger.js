8d63029dbd2a42db7b46f0af2d67261e1ff4d394
Alan Martines <alancpmartines@hotmail.com>
2023-03-21 14:04:21 -0400

    Update

--- swagger.js
+++ swagger.js
@@ -10,7 +10,7 @@ module.exports = {
 	"info": {
 		"description": "Esta Api, segue os mesmos termos de serviço do WhatsApp. É importante que você leia atentamente a estes termos. Você é responsável pelo uso da ferramenta e pelas conseqüências do mau uso. Reforçamos que a API não é destinada para prática de SPAM e que o envio de mensagens indesejadas, viola os termos de serviço do WhatsApp. A violação dos termos pode acarretar no bloqueio e banimento definitivo de sua conta no WhatsApp.",
 		"version": "1.0.3",
-		"title": "API - Connect Zap"
+		"title": "API - My WhatsApp"
 	},
 	"tags": [
 		{
