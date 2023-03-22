dc1649814ac63cb183e8a2cbca9c1a3eade0cb94
Alan Martines <alancpmartines@hotmail.com>
2023-03-22 15:48:52 -0400

    Update

--- engine.js
+++ engine.js
@@ -220,7 +220,7 @@ module.exports = class Instace {
 	static async Start(req, res, next) {
 		let SessionName = req?.body?.SessionName;
 		let data = Sessions?.getSession(SessionName);
-		logger?.info(`Data: ${data}`);
+		logger?.info(`Data: ${JSON.stringify(data, null, 2)}`);
 		if (data) {
 			await saudacao();
 			logger?.info(`- Carregando sessão`);
