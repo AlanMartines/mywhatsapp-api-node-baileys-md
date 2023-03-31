0c969af774ad831765a664271f15fee66599c5b1
Alan Martines <alancpmartines@hotmail.com>
2023-03-31 11:38:24 -0400

    Update

--- startup.js
+++ startup.js
@@ -9,68 +9,68 @@ const config = require('./config.global');
 const tokenPatch = parseInt(config.INDOCKER) ? path.join(config.PATCH_TOKENS, os.hostname()) : config.PATCH_TOKENS;
 
 if (!fs.existsSync(tokenPatch)) {
-  try {
-    fs.mkdirSync(tokenPatch, { recursive: true });
-  } catch (error) {
-    logger.error(`- Error creating directory ${tokenPatch}: ${error}`);
-  }
+	try {
+		fs.mkdirSync(tokenPatch, { recursive: true });
+	} catch (error) {
+		logger.error(`- Error creating directory ${tokenPatch}: ${error}`);
+	}
 }
 
 class AllSessions {
-  static async getAllSessions() {
-    let startup = [];
-    try {
-      const files = await fs.readdir(tokenPatch);
-      files.forEach(file => {
-        if (file.includes('.startup.json')) {
-          startup.push(file.split('.')[0]);
-        }
-      });
-    } catch (error) {
-      logger.error(`- Error getting sessions: ${error}`);
-    }
-    return startup;
-  }
-
-  static async startAllSessions() {
-    const hostUrl = config.HOST == '0.0.0.0' ? '127.0.0.1' : config.HOST;
-    const host = config.DOMAIN_SSL == '' ? `http://${hostUrl}:${config.PORT}` : `https://${config.DOMAIN_SSL}`;
-    const dados = await this.getAllSessions();
-		if(dados?.length){
-    for (let item of dados) {
-      setTimeout(async () => {
-        try {
-          const filePath = path.join(tokenPatch, `${item}.startup.json`);
-          const resBody = {};
-
-            let result = JSON.parse(await fs.readFile(filePath, 'utf-8'));
-            resBody.SessionName = item;
-            resBody.setOnline = result.setOnlineue;
-            resBody.wh_connect = result.wh_connect;
-            resBody.wh_qrcode = result.wh_qrcode;
-            resBody.wh_status = result.wh_status;
-            resBody.wh_message = result.wh_message;
-
-          const options = {
-            method: 'POST',
-            rejectUnauthorized: false,
-            headers: {
-              'Content-Type': 'application/json',
-              AuthorizationToken: parseInt(config.VALIDATE_MYSQL) ? item : config.SECRET_KEY
-            },
-            json: true,
-            url: `${host}/instance/Start`,
-            body: resBody
-          };
-          await request(options);
-          logger?.info(`- Start Session Name: ${item}`);
-        } catch (err) {
-          logger?.error(`- Error starting session ${item}: ${err}`);
-        }
-      }, 3000);
-    }
+	static async getAllSessions() {
+		let startup = [];
+		try {
+			const files = await fs.readdir(tokenPatch);
+			files.forEach(file => {
+				if (file.includes('.startup.json')) {
+					startup.push(file.split('.')[0]);
+				}
+			});
+		} catch (error) {
+			logger.error(`- Error getting sessions: ${error}`);
 		}
-  }
+		return startup;
+	}
+
+	static async startAllSessions() {
+		const hostUrl = config.HOST == '0.0.0.0' ? '127.0.0.1' : config.HOST;
+		const host = config.DOMAIN_SSL == '' ? `http://${hostUrl}:${config.PORT}` : `https://${config.DOMAIN_SSL}`;
+		const dados = await this.getAllSessions();
+		if (dados?.length) {
+			for (let item of dados) {
+				setTimeout(async () => {
+					try {
+						const filePath = path.join(tokenPatch, `${item}.startup.json`);
+						const resBody = {};
+
+						let result = JSON.parse(await fs.readFile(filePath, 'utf-8'));
+						resBody.SessionName = item;
+						resBody.setOnline = result.setOnlineue;
+						resBody.wh_connect = result.wh_connect;
+						resBody.wh_qrcode = result.wh_qrcode;
+						resBody.wh_status = result.wh_status;
+						resBody.wh_message = result.wh_message;
+
+						const options = {
+							method: 'POST',
+							rejectUnauthorized: false,
+							headers: {
+								'Content-Type': 'application/json',
+								AuthorizationToken: parseInt(config.VALIDATE_MYSQL) ? item : config.SECRET_KEY
+							},
+							json: true,
+							url: `${host}/instance/Start`,
+							body: resBody
+						};
+						await request(options);
+						logger?.info(`- Start Session Name: ${item}`);
+					} catch (err) {
+						logger?.error(`- Error starting session ${item}: ${err}`);
+					}
+				}, 3000);
+			}
+		}
+	}
 }
 
 module.exports = AllSessions;
