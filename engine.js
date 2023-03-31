3ffef440483a38f263c95a175e2e957f81e051a8
Alan Martines <alancpmartines@hotmail.com>
2023-03-31 14:58:06 -0400

    Update

--- engine.js
+++ engine.js
@@ -522,6 +522,9 @@ module.exports = class Instace {
 								//
 								logger?.info("- Captura do QR-Code");
 								//
+								webhooks?.wh_qrcode(SessionName, readQRCode, qr);
+								this.exportQR(req.io, readQRCode, SessionName, attempts);
+								//
 								if (parseInt(config.VIEW_QRCODE_TERMINAL)) {
 									qrViewer.generate(qr, { small: true });
 								}
@@ -537,18 +540,6 @@ module.exports = class Instace {
 								await Sessions?.addInfoSession(SessionName, addJson);
 								//
 								await updateStateDb(addJson?.state, addJson?.status, SessionName);
-								//
-								webhooks?.wh_qrcode(SessionName, readQRCode, qr);
-								//this.exportQR(funcoesSocket, readQRCode, SessionName, attempts, addJson?.state, addJson?.status);
-								//
-								funcoesSocket.qrCode(SessionName, {
-										SessionName: SessionName,
-										data: base64Code,
-										attempts: attempts,
-										message: addJson?.message,
-										state: addJson?.state,
-										status: addJson?.status
-								});
 								//
 								if (attempts >= 5) {
 									//
@@ -1075,7 +1066,16 @@ module.exports = class Instace {
 		//
 	}
 	//
-	static async exportQR(funcoesSocket, base64Code, SessionName, attempts, state, status) {
-
+	static async exportQR(socket, base64Code, SessionName, attempts) {
+		//
+		socket.emit('qrCode',
+			{
+				data: base64Code,
+				SessionName: SessionName,
+				attempts: attempts,
+				message: 'QRCode Iniciado, Escanei por favor...'
+			}
+		);
+		//
 	};
 }
