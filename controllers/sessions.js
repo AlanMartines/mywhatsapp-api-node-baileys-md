cafc066189d7c7e2418c13723b8373a0067d3573
Alan Martines <alancpmartines@hotmail.com>
2023-03-31 10:50:31 -0400

    Update

--- controllers/sessions.js
+++ controllers/sessions.js
@@ -64,7 +64,7 @@ module.exports = class Sessions {
 
   // pegar index da sessão (chave)
   static async getSessionKey(name) {
-    if (this.checkSession(name)) {
+    if (await this.checkSession(name)) {
       for (var i in this.session) {
         if (this.session[i]?.session === name) {
           return i
@@ -76,7 +76,7 @@ module.exports = class Sessions {
 
   // adicionar informações a sessão 
   static async addInfoSession(name, extend) {
-    if (this.checkSession(name)) {
+    if (await this.checkSession(name)) {
       for (var i in this.session) {
         if (this.session[i]?.session === name) {
           Object?.assign(this.session[i], extend);
@@ -89,7 +89,7 @@ module.exports = class Sessions {
 
   // Remove object na sessão
   static async removeInfoObjects(name, key) {
-    if (this.checkSession(name)) {
+    if (await this.checkSession(name)) {
       for (var i in this.session) {
         if (this.session[i]?.session === name) {
           delete this.session[i][key]
@@ -102,8 +102,8 @@ module.exports = class Sessions {
 
   // deletar sessão
   static async deleteSession(name) {
-    if (this.checkSession(name)) {
-      var key = this.getSessionKey(name)
+    if (await this.checkSession(name)) {
+      var key = await this.getSessionKey(name)
       delete this.session[key]
       return true
     }
@@ -112,9 +112,8 @@ module.exports = class Sessions {
 
   // retornar sessão
   static async getSession(name) {
-    if (this.checkSession(name)) {
-      var key = this.getSessionKey(name);
-			console.log(this.session[key]);
+    if (await this.checkSession(name)) {
+      var key = await this.getSessionKey(name);
       return this.session[key];
     }
     return false
@@ -127,7 +126,7 @@ module.exports = class Sessions {
 
   // checa o client
   static async checkClient(name) {
-    if (this.getSession(name) && this.getSession(name)?.client) {
+    if (await this.getSession(name) && await this.getSession(name)?.client) {
       return true
     }
     return false
