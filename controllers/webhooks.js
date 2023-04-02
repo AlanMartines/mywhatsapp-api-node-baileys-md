da1a9fc373c2fd8d96f660b5b30d7210c91b8058
Alan Martines <alancpmartines@hotmail.com>
2023-04-02 00:23:02 -0400

    Update

--- controllers/webhooks.js
+++ controllers/webhooks.js
@@ -14,6 +14,7 @@ module.exports = class Webhooks {
 			if (dataSessions?.wh_message != undefined && dataSessions?.wh_message != null && dataSessions?.wh_message != '') {
 				logger.info(`- SessionName: ${SessionName}`);
 				let dataJson = JSON.stringify(object, null, 2);
+				/*
 				await axios.post(dataSessions?.wh_message, dataJson, {
 					httpsAgent: new https.Agent({
 						rejectUnauthorized: false,
@@ -25,7 +26,7 @@ module.exports = class Webhooks {
 				}).catch(error => {
 					logger?.error(`- Error receive message: ${error.message}`);
 				});
-
+				*/
 
 				const objeto = {
 					"nome": "João",
