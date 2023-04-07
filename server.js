c371aa41af0d0c2454c74f035bcbf3fb98be9124
Alan Martines <alancpmartines@hotmail.com>
2023-04-07 11:18:48 -0400

    Update

--- server.js
+++ server.js
@@ -46,7 +46,7 @@ logger?.info(
 );
 //
 //
-logger.info(boxen('unicorn', {padding: 1}));
+console.log(boxen('unicorn', {padding: 1}));
 //
 //
 // ------------------------------------------------------------------------------------------------//
