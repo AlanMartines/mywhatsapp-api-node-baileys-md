70f143f85f866f201d3700d68d88f38358a11976
Alan Martines <alancpmartines@hotmail.com>
2023-05-11 09:29:55 -0400

    Update

--- controllers/events.js
+++ controllers/events.js
@@ -13,13 +13,13 @@ moment?.locale('pt-br');
 const convertBytes = async function (bytes) {
 	const sizes = ["Bytes", "KB", "MB", "GB", "TB"]
 	if (bytes == 0) {
-		return "n/a"
+		return null;
 	}
-	const i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)))
+	const i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)));
 	if (i == 0) {
-		return bytes + " " + sizes[i]
+		return bytes + " " + sizes[i];
 	}
-	return (bytes / Math.pow(1024, i)).toFixed(1) + " " + sizes[i]
+	return (bytes / Math.pow(1024, i)).toFixed(1) + " " + sizes[i];
 }
 //
 // ------------------------------------------------------------------------------------------------------- //
