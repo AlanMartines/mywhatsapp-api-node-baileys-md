ae35e6a17f0315d6f6d9dfdcfed7da0f8f332141
Alan Martines <alancpmartines@hotmail.com>
2023-05-16 09:15:06 -0400

    Update

--- startup.js
+++ startup.js
@@ -18,7 +18,7 @@ if (!fs.existsSync(tokenPatch)) {
 
 class AllSessions {
 	static async getAllSessions() {
-		logger?.info(` - Getting sessions to auto start`);
+		logger?.info(`- Getting sessions to auto start`);
 		let startup = [];
 		try {
 			const files = await fs.readdir(tokenPatch);
