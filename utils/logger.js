03e220ebb990d1e3f20323b34422c6e466e97f85
Alan Martines <alancpmartines@hotmail.com>
2023-06-21 08:31:25 -0400

    Update

--- utils/logger.js
+++ utils/logger.js
@@ -1,7 +1,7 @@
-/*
 const pino = require("pino");
 const pretty = require('pino-pretty');
-
+const config = require('../config.global');
+//
 const logger = pino({
 	timestamp: false,
 	levelFirst: true,
@@ -41,42 +41,5 @@ const logger = pino({
 		},
 	},
 });
-
-exports.logger = logger;
-*/
-
-const pino = require('pino');
-const config = require('../config.global');
-
-const gelfStream = require('pino-gelf')({
-      host: config.GRAYLOGSERVER, // substitua por seu host Graylog
-      port: config.GRAYLOGPORT, // substitua pela sua porta Graylog, se diferente
-});
-
-const logger = pino({
-  timestamp: false,
-  levelFirst: true,
-  messageKey: 'message',
-  formatters: {
-    level(label, number) {
-      return { level: label };
-    },
-    log(object) {
-      if (object && object.stack) {
-        const stackTrace = object.stack.split('\n');
-        const fileLine = stackTrace[1].match(/\(([^)]+)\)/)[1];
-        const fileName = fileLine.split(':')[0];
-        const lineNumber = fileLine.split(':')[1];
-        return {
-          stack: object.stack,
-          message: object.message,
-          line: lineNumber,
-          file: fileName
-        };
-      }
-      return object;
-    },
-  },
-}, gelfStream);
-
+//
 exports.logger = logger;
