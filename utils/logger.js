e055274933a887e240cd482051746d4f4e139377
Alan Martines <alancpmartines@hotmail.com>
2023-06-21 08:07:07 -0400

    Update

--- utils/logger.js
+++ utils/logger.js
@@ -1,3 +1,4 @@
+/*
 const pino = require("pino");
 const pretty = require('pino-pretty');
 
@@ -40,5 +41,40 @@ const logger = pino({
 		},
 	},
 });
+
+exports.logger = logger;
+*/
+const pino = require('pino');
+const gelfStream = require('gelf-stream');
+const config = require('../config.global');
+
+const stream = gelfStream.forBunyan(config.GRAYLOGSERVER, config.GRAYLOGPORT); // Substitua 'localhost' e '12201' pelo seu host e porta do Graylog
+
+const logger = pino({
+    level: 'info',
+    messageKey: 'message',
+    formatters: {
+        level(label, number) {
+            return { level: label };
+        },
+        log(object) {
+            if (object.err && object.err.stack) {
+                const stackTrace = object.err.stack.split('\n');
+                const fileLine = stackTrace[1].match(/\(([^)]+)\)/)[1];
+                const fileName = fileLine.split(':')[0];
+                const lineNumber = fileLine.split(':')[1];
+                //
+                return {
+                    stack: object.err.stack,
+                    message: object.err.message,
+                    line: lineNumber,
+                    file: fileName
+                };
+            }
+            //
+            return object;
+        },
+    },
+}, stream);
 
 exports.logger = logger;
