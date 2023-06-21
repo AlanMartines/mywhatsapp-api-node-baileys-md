62aa7242fe2ed579d2ecd36ce964e7defca92942
Alan Martines <alancpmartines@hotmail.com>
2023-06-21 08:40:01 -0400

    Update

--- utils/logger.js
+++ utils/logger.js
@@ -1,3 +1,4 @@
+/*
 const pino = require("pino");
 const pretty = require('pino-pretty');
 const config = require('../config.global');
@@ -43,6 +44,60 @@ const logger = pino({
 	},
 });
 //
-//module.exports = { logger };
-//
 exports.logger = logger;
+*/
+const pino = require("pino");
+const gelfPro = require('gelf-pro');
+const config = require('../config.global');
+
+// Configuração do gelf-pro para o Graylog
+gelfPro.setConfig({
+  host: config.GRAYLOGSERVER,
+  port: config.GRAYLOGPORT,
+  protocol: 'udp', // ou 'tcp'
+  adapterName: 'udp', // ou 'tcp'
+  adapterOptions: {
+    family: 4,
+		host: config.GRAYLOGSERVER,
+		port: config.GRAYLOGPORT,
+    exclusive: false
+  }
+});
+
+const logger = pino({
+  timestamp: false,
+  levelFirst: true,
+  formatters: {
+    level(label, number) {
+      return { level: label };
+    },
+    log(object) {
+      gelfPro.info(object);
+      return object;
+    },
+    error(error) {
+      if (error && error.stack) {
+        const stackTrace = error.stack.split('\n');
+        let fileLine, fileName, lineNumber;
+        try {
+          fileLine = stackTrace[1].match(/\(([^)]+)\)/)[1];
+          [fileName, lineNumber] = fileLine.split(':');
+        } catch (err) {
+          console.error("Erro ao processar a stack trace", err);
+        }
+        const logObject = {
+          stack: error.stack,
+          message: error.message,
+          line: lineNumber,
+          file: fileName
+        };
+        gelfPro.error(logObject);
+        return logObject;
+      }
+      gelfPro.error(error);
+      return error;
+    },
+  },
+});
+
+module.exports = logger ;
