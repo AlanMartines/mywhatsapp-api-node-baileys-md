754a4e1786b17bb96de0271e8328a561e6333c9f
Alan Martines <alancpmartines@hotmail.com>
2023-06-21 08:30:33 -0400

    Update

--- utils/logger.js
+++ utils/logger.js
@@ -54,41 +54,29 @@ const gelfStream = require('pino-gelf')({
 });
 
 const logger = pino({
-  transport: {
-		target: 'pino-pretty',
-		options: {
-			errorProps: '*',
-			ignore: "pid,hostname",
-			translateTime: "SYS:yyyy-mm-dd HH:MM:ss",
-			colorize: true
-		}
-	}
-}, gelfStream, {
-	// Opções de log de erro
-	level: 'error',
-	messageKey: 'message',
-	formatters: {
-		level(label, number) {
-			return { level: label };
-		},
-		error(error) {
-			if (error && error.stack) {
-				const stackTrace = error.stack.split('\n');
-				const fileLine = stackTrace[1].match(/\(([^)]+)\)/)[1];
-				const fileName = fileLine.split(':')[0];
-				const lineNumber = fileLine.split(':')[1];
-				//
-				return {
-					stack: error.stack,
-					message: error.message,
-					line: lineNumber,
-					file: fileName
-				};
-			}
-			//
-			return error;
-		},
-	},
-});
+  timestamp: false,
+  levelFirst: true,
+  messageKey: 'message',
+  formatters: {
+    level(label, number) {
+      return { level: label };
+    },
+    log(object) {
+      if (object && object.stack) {
+        const stackTrace = object.stack.split('\n');
+        const fileLine = stackTrace[1].match(/\(([^)]+)\)/)[1];
+        const fileName = fileLine.split(':')[0];
+        const lineNumber = fileLine.split(':')[1];
+        return {
+          stack: object.stack,
+          message: object.message,
+          line: lineNumber,
+          file: fileName
+        };
+      }
+      return object;
+    },
+  },
+}, gelfStream);
 
 exports.logger = logger;
