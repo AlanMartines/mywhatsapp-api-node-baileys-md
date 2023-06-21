220eafd6518e41e356f97c71533d963abbdb42c0
Alan Martines <alancpmartines@hotmail.com>
2023-06-21 08:57:08 -0400

    Update

--- utils/logger.js
+++ utils/logger.js
@@ -1,4 +1,4 @@
-/*
+
 const pino = require("pino");
 const pretty = require('pino-pretty');
 const config = require('../config.global');
@@ -45,59 +45,3 @@ const logger = pino({
 });
 //
 exports.logger = logger;
-*/
-const pino = require("pino");
-const gelf = require('pino-gelf');
-const config = require('../config.global');
-
-const logger = pino({
-	timestamp: false,
-	levelFirst: true,
-	messageKey: 'message',
-	formatters: {
-		level(label, number) {
-			return { level: label };
-		},
-		log(object) {
-			return {
-				version: '1.1',
-				host: 'example.com',
-				short_message: object.message,
-				full_message: JSON.stringify(object),
-				timestamp: object.time,
-				level: object.level,
-				_file: object.file,
-				_line: object.line,
-				...object
-			};
-		},
-		error(error) {
-			if (error && error.stack) {
-				const stackTrace = error.stack.split('\n');
-				let fileLine, fileName, lineNumber;
-				try {
-					fileLine = stackTrace[1].match(/\(([^)]+)\)/)[1];
-					[fileName, lineNumber] = fileLine.split(':');
-				} catch (err) {
-					console.error("Erro ao processar a stack trace", err);
-				}
-				return {
-					stack: error.stack,
-					message: error.message,
-					line: lineNumber,
-					file: fileName
-				};
-			}
-			return error;
-		},
-	},
-});
-
-const gelfStream = gelf.createWriteStream({
-	host: config.GRAYLOGSERVER,
-	port: config.GRAYLOGPORT
-});
-
-logger.pipe(gelfStream);
-
-module.exports = logger;
