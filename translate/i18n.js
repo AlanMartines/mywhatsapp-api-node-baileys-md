d254521d90507cbf257e0a45b3c2a9dd476e6d62
Alan Martines <alancpmartines@hotmail.com>
2023-11-02 19:27:06 -0400

    Update

--- translate/i18n.js
+++ translate/i18n.js
@@ -1,10 +1,11 @@
 const i18n = require('i18n');
 const config = require('../config.global');
+//
 i18n.configure({
     locales: ['pt-br', 'en'],
     directory: __dirname + '/languages',
     defaultLocale: 'en',
     objectNotation: true,
 });
-
+//
 module.exports = i18n;
