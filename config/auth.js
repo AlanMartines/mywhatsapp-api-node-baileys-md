2fef55193992ed39d8e860841d6a5583f8cd5517
Alan Martines <alancpmartines@hotmail.com>
2023-11-15 11:06:25 -0400

    Update

---
+++ config/auth.js
@@ -0,0 +1,6 @@
+module.exports = {
+  secret: "kZaOTdYZpjRUyyUpigJaEMk4vcW4YOymKPZX0Ts8",
+  expiresIn: "1d",
+  refreshSecret: "dBSXqFg9TaNUXVp6fhMTRLBysPj2DSqf7raxD3A",
+  refreshExpiresIn: "7d"
+};
